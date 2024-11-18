e9f414e86c84029aa058f46e6a12903a1768e5e0
flagmanAndrew <andrey.sergeyev@devexpress.com>
2024-11-18 21:15:50 +0400

    ViewComponents navigation

---
+++ ViewComponents/AboutViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace devextreme_asp_ui_template_gallery.ViewComponents
+{
+    public class AboutViewComponent : ViewComponent
+    {
+        public AboutViewComponent() {
+        }
+        public IViewComponentResult Invoke() {
+            return View();
+        }
+    }
+}
---
+++ ViewComponents/TasksViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace devextreme_asp_ui_template_gallery.ViewComponents
+{
+    public class TasksViewComponent : ViewComponent
+    {
+        public TasksViewComponent() {
+        }
+        public IViewComponentResult Invoke() {
+            return View();
+        }
+    }
+}
