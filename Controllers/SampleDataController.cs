a59fa4e71d0055be5e185cde4816d2908d361bd9
DuckTieCorpMember <danyshbek1996@gmail.com>
2024-11-19 09:59:47 +0500

    Add obj to gitignore and change the namespace

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using devextreme_asp_ui_template_gallery.Models;
 using DevExtreme.AspNet.Data;
 using DevExtreme.AspNet.Mvc;
 using Microsoft.AspNetCore.Mvc;
+using DevExtreme.Asp.Template.Gallery.Models;
 
-namespace devextreme_asp_ui_template_gallery.Controllers {
+namespace DevExtreme.Asp.Template.Gallery.Controllers
+{
 
     [Route("api/[controller]")]
-    public class SampleDataController : Controller {
+    public class SampleDataController : Controller
+    {
 
         [HttpGet]
-        public object Get(DataSourceLoadOptions loadOptions) {
+        public object Get(DataSourceLoadOptions loadOptions)
+        {
             return DataSourceLoader.Load(SampleData.Orders, loadOptions);
         }
 
