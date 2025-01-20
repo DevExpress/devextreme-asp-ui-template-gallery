9b7f7d1ab107108b9f73c8b31c562bcf2d570565
flagmanAndrew <andrey.sergeyev@devexpress.com>
2025-01-20 22:25:36 +0400

    Fetch Data From Remote Service

---
+++ Utils/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace devextreme_asp_ui_template_gallery.Utils
+{
+    public static class Constants {
+        public static string BaseUrlAPI = "https://js.devexpress.com/Demos/RwaService/api";
+    }
+}
