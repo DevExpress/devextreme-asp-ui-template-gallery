76d30dd13f46e561fc00c20aca0294b16797d431
Tom <artem.kurchenko@devexpress.com>
2025-07-01 15:46:11 +0400

    re-write ThemeController to iife

--- src/ts/theme.ts
+++ src/ts/theme.ts
@@ -1,27 +1,35 @@
-class ThemeController {
- 
-  init() {
-    DevExpress.ui.themes.current(window.localStorage.getItem("dx-theme") || "fluent.blue.light");
-    window.localStorage.setItem("dx-theme", DevExpress.ui.themes.current());
-  }
+(function () {
+    if (window.uitgAppContext?.ThemeController) return;
 
-  getTheme() {
-    return DevExpress.ui.themes.current();
-  }
-
-  themeButtonOnInitialized = (e: DevExpress.ui.dxButton.InitializedEvent) => {
-    const icon = this.getTheme() === "fluent.blue.light" ? "moon" : "sun";
-    e.component?.option("icon", icon);
-  }
-
-  themeSwitcherOnClick = (e: DevExpress.ui.dxButton.ClickEvent) => {
-    if (this.getTheme() === "fluent.blue.light") {
-      e.component.option("icon", "sun");
-      DevExpress.ui.themes.current("fluent.blue.dark");
-    } else {
-      e.component.option("icon", "moon");
-      DevExpress.ui.themes.current("fluent.blue.light");
+    function init() {
+        DevExpress.ui.themes.current(window.localStorage.getItem("dx-theme") || "fluent.blue.light");
+        window.localStorage.setItem("dx-theme", DevExpress.ui.themes.current());
     }
-    window.localStorage.setItem("dx-theme",  this.getTheme());
-  }
-}
+
+    function getTheme() {
+        return DevExpress.ui.themes.current();
+    }
+
+    function themeButtonOnInitialized(e: any) {
+        const icon = getTheme() === "fluent.blue.light" ? "moon" : "sun";
+        e.component?.option("icon", icon);
+    }
+
+    function themeSwitcherOnClick(e: any) {
+        if (getTheme() === "fluent.blue.light") {
+            e.component.option("icon", "sun");
+            DevExpress.ui.themes.current("fluent.blue.dark");
+        } else {
+            e.component.option("icon", "moon");
+            DevExpress.ui.themes.current("fluent.blue.light");
+        }
+        window.localStorage.setItem("dx-theme", getTheme());
+    }
+
+    window.uitgAppContext.ThemeController = {
+        init,
+        getTheme,
+        themeButtonOnInitialized,
+        themeSwitcherOnClick
+    };
+})();
