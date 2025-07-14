83d6bacb301d808a037ffc3a9c5d5e8eb2f0eb1b
Tom <22076961+artem-kurchenko@users.noreply.github.com>
2025-07-14 17:24:06 +0400

    Planning Tasks - Add toolbar actions (#25)
    * add exports and functions

--- src/ts/views/PlanningTaskList.ts
+++ src/ts/views/PlanningTaskList.ts
@@ -27,23 +27,64 @@
     }
 
     function reload() {
-        // Implementation for reload
+        if (currentView === 'Grid') {
+            $('#tasks-grid').dxDataGrid('instance').refresh();
+        } else if(currentView === 'Gantt') {
+            $('#tasks-gantt').dxGantt('instance').refresh();
+        }else if(currentView === 'Kanban') {
+            $('#kanban-scroll-view').dxScrollView('instance').update();
+        }
     }
 
     function chooseColumnDataGrid() {
-        // Implementation for chooseColumnDataGrid
+        $('#tasks-grid').dxDataGrid('instance').showColumnChooser();
     }
 
     function exportToPdf() {
-        // Implementation for exportToPdf
+        // @ts-expect-error no typings included
+        window.jsPDF = window.jspdf.jsPDF;
+        // @ts-expect-error no typings included
+        applyPlugin(window.jsPDF);
+        let currentView = getCurrentView();
+        if (currentView === 'Grid') {
+            // @ts-expect-error no typings included
+            const doc = new jsPDF();
+            DevExpress.pdfExporter.exportDataGrid({
+                jsPDFDocument: doc,
+                component: $('#tasks-grid').dxDataGrid('instance'),
+            }).then(() => {
+                doc.save('Tasks.pdf');
+            });
+        } else {
+            DevExpress.pdfExporter.exportGantt(
+                {
+                    component: $('#tasks-gantt').dxGantt('instance'),
+                    // @ts-expect-error no typings included
+                    createDocumentMethod: (args) => new jsPDF(args),
+                },
+            ).then((doc) => doc.save('gantt.pdf'));
+        }
     }
 
     function exportToXlsx() {
-        // Implementation for exportToXlsx
+        // @ts-expect-error no typings included
+        const workbook = new ExcelJS.Workbook();
+        const worksheet = workbook.addWorksheet('Main sheet');
+
+        DevExpress.excelExporter.exportDataGrid({
+            component: $('#tasks-grid').dxDataGrid('instance'),
+            worksheet,
+            autoFilterEnabled: true,
+        }).then(() => {
+            workbook.xlsx.writeBuffer().then((buffer: any) => {
+                // @ts-expect-error no typings included
+                saveAs(new Blob([buffer], { type: 'application/octet-stream' }), 'DataGrid.xlsx');
+            });
+        });
     }
 
-    function searchDataGrid(e: any) {
-        // Implementation for searchDataGrid
+    function searchDataGrid(e: DevExpress.ui.dxTextBox.ValueChangedEvent) {
+        $('#tasks-grid').dxDataGrid('instance').searchByText(e.component.option('text') ?? '');
     }
 
     window.uitgAppContext.PlanningTasksController = {
